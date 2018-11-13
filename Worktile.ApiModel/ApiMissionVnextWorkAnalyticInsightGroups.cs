321456e0ffbda00fc3c184d683b9638954631b3d
HeroWong <hero_wong@outlook.com>
2018-11-13 23:22:50 +0800

    index page

---
+++ Worktile.ApiModel/ApiMissionVnextWorkAnalyticInsightGroups.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Worktile.ApiModel.ApiMissionVnextWorkAnalyticInsightGroups
+{
+
+    public partial class ApiMissionVnextWorkAnalyticInsightGroups
+    {
+        [JsonProperty("oid")]
+        public string Oid { get; set; }
+
+        [JsonProperty("code")]
+        public long Code { get; set; }
+
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+    }
+
+    public partial class Data
+    {
+        [JsonProperty("value")]
+        public List<Value> Value { get; set; }
+    }
+
+    public partial class Value
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("position")]
+        public long Position { get; set; }
+
+        [JsonProperty("insights")]
+        public List<Insight> Insights { get; set; }
+    }
+
+    public partial class Insight
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("insight_id")]
+        public string InsightId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("is_global")]
+        public long IsGlobal { get; set; }
+    }
+}
--- Worktile/Views/Mission/AnalyticInsight/IndexPage.xaml.cs
+++ Worktile/Views/Mission/AnalyticInsight/IndexPage.xaml.cs
@@ -10,21 +10,113 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using System.ComponentModel;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+using Worktile.ApiModel.ApiMissionVnextWorkAddon;
+using Worktile.Services;
+using System.Threading.Tasks;
+using Worktile.WtRequestClient;
+using Worktile.ApiModel.ApiMissionVnextWorkAnalyticInsightGroups;
+using System.Collections.ObjectModel;
 
 namespace Worktile.Views.Mission.AnalyticInsight
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
-    public sealed partial class IndexPage : Page
+    public sealed partial class IndexPage : Page, INotifyPropertyChanged
     {
         public IndexPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            TopNavItems = new ObservableCollection<TopNavItem>();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ApiModel.ApiMissionVnextWorkAddon.Value _navItem;
+
+        public ObservableCollection<TopNavItem> TopNavItems { get; }
+
+        private string _topNavIcon;
+        public string TopNavIcon
+        {
+            get => _topNavIcon;
+            set
+            {
+                _topNavIcon = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TopNavIcon)));
+            }
+        }
+
+        private string _topNavName;
+        public string TopNavName
+        {
+            get => _topNavName;
+            set
+            {
+                _topNavName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TopNavName)));
+            }
+        }
+
+        private TopNavItem _selectedNav;
+        public TopNavItem SelectedNav
+        {
+            get => _selectedNav;
+            set
+            {
+                if (SelectedNav != value)
+                {
+                    _selectedNav = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedNav)));
+                    //FrameNavigate();
+                }
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            _navItem = e.Parameter as ApiModel.ApiMissionVnextWorkAddon.Value;
+            TopNavIcon = WtIconHelper.GetGlyph(_navItem.Icon);
+            TopNavName = _navItem.Name;
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await RequestApiMissionVnextWorkAnalyticInsightGroups();
+        }
+
+        private async Task RequestApiMissionVnextWorkAnalyticInsightGroups()
+        {
+            string uri = "/api/mission-vnext/work/analytic-insight/groups";
+            var client = new WtHttpClient();
+            var data = await client.GetAsync<ApiMissionVnextWorkAnalyticInsightGroups>(uri);
+            foreach (var item in data.Data.Value)
+            {
+                var tni = new TopNavItem
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    SubItems = new List<TopNavItem>()
+                };
+                foreach (var ins in item.Insights)
+                {
+                    tni.SubItems.Add(new TopNavItem
+                    {
+                        Id = ins.Id,
+                        Name = ins.Name
+                    });
+                }
+                TopNavItems.Add(tni);
+            }
+            SelectedNav = TopNavItems.First();
+        }
+    }
+
+    public class TopNavItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<TopNavItem> SubItems { get; set; }
     }
 }
