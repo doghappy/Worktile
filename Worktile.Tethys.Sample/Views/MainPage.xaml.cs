05df56f7dae58d2623e99856c0afb082b072045b
HeroWong <hero_wong@outlook.com>
2018-12-20 20:02:16 +0800

    add tethys

---
+++ Worktile.Tethys.Sample/Views/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Worktile.Tethys.Sample.Models;
+
+namespace Worktile.Tethys.Sample.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Navs = new List<NavItem>
+            {
+                new NavItem
+                {
+                    Name = "Basic",
+                    Glyph = "\uE10F",
+                    Children = new List<NavItem>
+                    {
+                        new NavItem
+                        {
+                            Name = "Color",
+                            Glyph = "\ue790",
+                            SourcePageType = typeof(Basic.ColorPage)
+                        },
+                        new NavItem
+                        {
+                            Name = "Wtf",
+                            Glyph = "\ue164",
+                            SourcePageType = typeof(Basic.WtfPage)
+                        }
+                    }
+                },
+                new NavItem
+                {
+                    Name = "Data Display",
+                    Glyph = "\uE7BC",
+                    Children = new List<NavItem>
+                    {
+                        new NavItem
+                        {
+                            Name = "Label",
+                            Glyph = "\ue930",
+                            SourcePageType = typeof(DataDisplay.LabelPage)
+                        }
+                    }
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        List<NavItem> Navs { get; }
+
+        NavItem _selectedNav;
+        NavItem SelectedNav
+        {
+            get => _selectedNav;
+            set
+            {
+                if (_selectedNav != value)
+                {
+                    _selectedNav = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedNav)));
+                    ContentFrame.Navigate(typeof(GridViewPage), value.Children);
+                }
+            }
+        }
+
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                ContentFrame.GoBack();
+            }
+        }
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            SelectedNav = Navs.First();
+        }
+    }
+}
---
+++ Worktile/WtControls/MultipleComboBox.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Worktile.WtControls
+{
+    public sealed partial class MultipleComboBox : UserControl
+    {
+        public MultipleComboBox()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public DataTemplate ListItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(MultipleComboBox), new PropertyMetadata(null));
+
+
+
+        private void Flyout_Opened(object sender, object e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listView = sender as ListView;
+
+        }
+    }
+}
