8e8a68fde447885d231f05e4852a8f5d4467cf77
HeroWong <hero_wong@outlook.com>
2019-01-10 11:55:48 +0800

    尝试分离ViewModel前

--- Worktile/Views/IM/ChannelMessagePage.xaml.cs
+++ Worktile/Views/IM/ChannelMessagePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Collections;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -121,5 +122,10 @@ namespace Worktile.Views.IM
                 }
             }
         }
+    }
+
+    class MessageSource : IIncrementalSource<Message>
+    {
+
     }
 }
