44513f047bc551486cb5400b8119df5b464025d3
HeroWong <hero_wong@outlook.com>
2019-02-12 16:55:38 +0800

    pivot to nav

---
+++ Worktile/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+using Worktile.ViewModels;
+
+namespace Worktile.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+        }
+
+        public MainViewModel ViewModel { get; }
+    }
+}
