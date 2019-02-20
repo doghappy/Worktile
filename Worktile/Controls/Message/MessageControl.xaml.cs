80d33e3e2c81e1cc69c9458dc1e7d99f2481da5d
HeroWong <hero_wong@outlook.com>
2019-02-20 16:18:34 +0800

    add msg control

---
+++ Worktile/Controls/Message/MessageControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Worktile.Views.Message;
+
+namespace Worktile.Controls
+{
+    public sealed partial class MessageControl : UserControl
+    {
+        public MessageControl()
+        {
+            InitializeComponent();
+        }
+
+        public Message Message
+        {
+            get { return (Message)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(int), typeof(MessageControl), new PropertyMetadata(null));
+    }
+}
