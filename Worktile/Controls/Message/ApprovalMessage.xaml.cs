d604fa0fa2ae93e2366de26eaa7f8bb874eb58a8
HeroWong <hero_wong@outlook.com>
2019-02-28 21:40:42 +0800

    approval message template

---
+++ Worktile/Controls/Message/ApprovalMessage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
+using Worktile.Enums.Message;
+
+namespace Worktile.Controls.Message
+{
+    public sealed partial class ApprovalMessage : UserControl, INotifyPropertyChanged
+    {
+        public ApprovalMessage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Models.Message.Message _message;
+        public Models.Message.Message Message
+        {
+            get => _message;
+            set
+            {
+                if (_message != value && value.Type == MessageType.Approval)
+                {
+                    _message = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Models.Message.Message)));
+                }
+            }
+        }
+    }
+}
