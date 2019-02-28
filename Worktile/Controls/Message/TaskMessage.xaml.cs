02b2fbaf63377d3c77aebacd2eef04e5c3df5adb
HeroWong <hero_wong@outlook.com>
2019-02-28 14:50:15 +0800

    task message template

---
+++ Worktile/Controls/Message/TaskMessage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Worktile.Common;
+using Worktile.Enums.Message;
+
+namespace Worktile.Controls.Message
+{
+    public sealed partial class TaskMessage : UserControl, INotifyPropertyChanged
+    {
+        public TaskMessage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Models.Message.Message _message;
+        public Models.Message.Message Message
+        {
+            get => _message;
+            set
+            {
+                if (_message != value && value.Type == MessageType.Task)
+                {
+                    _message = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Models.Message.Message)));
+                }
+            }
+        }
+    }
+
+    //public class CalendarBackgroundConverter : IValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, string language)
+    //    {
+    //        string val = value.ToString();
+    //        if (val == "1")
+    //        {
+    //            return WtColorHelper.WarningColor1A;
+    //        }
+    //        return new SolidColorBrush(Colors.Transparent);
+    //    }
+
+    //    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
