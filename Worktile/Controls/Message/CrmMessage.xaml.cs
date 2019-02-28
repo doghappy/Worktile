c6b5d4670d0954b5aff7825b35c57aa15f1e8d61
HeroWong <hero_wong@outlook.com>
2019-02-28 16:00:13 +0800

    crm message template

---
+++ Worktile/Controls/Message/CrmMessage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Windows.UI.Xaml.Controls;
+using Worktile.Enums.Message;
+
+namespace Worktile.Controls.Message
+{
+    public sealed partial class CrmMessage : UserControl, INotifyPropertyChanged
+    {
+        public CrmMessage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Models.Message.Message _message;
+        public Models.Message.Message Message
+        {
+            get => _message;
+            set
+            {
+                if (_message != value && value.Type == MessageType.Crm)
+                {
+                    _message = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Models.Message.Message)));
+                }
+            }
+        }
+    }
+}
