7fde7c92ddc1d077e7f8bfdaa7617f22d0acc43a
HeroWong <hero_wong@outlook.com>
2019-03-01 10:30:58 +0800

    icon put on local

--- Worktile/Common/WtFileHelper.cs
+++ Worktile/Common/WtFileHelper.cs
@@ -4,7 +4,7 @@
     {
         public static string GetFileIcon(string ext)
         {
-            string name = ext;
+            string name = "default";
             switch (ext)
             {
                 case "doc":
@@ -19,8 +19,112 @@
                 case "xlsx":
                     name = "xls";
                     break;
+                case "jpg":
+                case "jpeg":
+                case "png":
+                case "gif":
+                case "bmp":
+                    name = "img";
+                    break;
+                case "apk":
+                    name = "apk";
+                    break;
+                case "bak":
+                    name = "bak";
+                    break;
+                case "box":
+                    name = "box";
+                    break;
+                case "cs":
+                    name = "cs";
+                    break;
+                case "csv":
+                    name = "csv";
+                    break;
+                case "evernote":
+                    name = "evernote";
+                    break;
+                case "exe":
+                    name = "exe";
+                    break;
+                case "fla":
+                    name = "fla";
+                    break;
+                case "html":
+                    name = "html";
+                    break;
+                case "ipa":
+                    name = "ipa";
+                    break;
+                case "java":
+                case "jsp":
+                    name = "java";
+                    break;
+                case "js":
+                    name = "js";
+                    break;
+                case "mp3":
+                    name = "mp3";
+                    break;
+                case "mp4":
+                    name = "mp4";
+                    break;
+                case "onedrive":
+                    name = "onedrive";
+                    break;
+                case "onenote":
+                    name = "onenote";
+                    break;
+                case "page":
+                    name = "page";
+                    break;
+                case "pdf":
+                    name = "pdf";
+                    break;
+                case "php":
+                    name = "php";
+                    break;
+                case "processon":
+                    name = "processon";
+                    break;
+                case "quip":
+                    name = "quip";
+                    break;
+                case "rar":
+                    name = "rar";
+                    break;
+                case "shimo":
+                    name = "shimo";
+                    break;
+                case "snippet":
+                    name = "snippet";
+                    break;
+                case "swf":
+                    name = "swf";
+                    break;
+                case "ttf":
+                    name = "ttf";
+                    break;
+                case "txt":
+                    name = "txt";
+                    break;
+                case "vss":
+                    name = "vss";
+                    break;
+                case "xsd":
+                    name = "xsd";
+                    break;
+                case "yinxiang":
+                    name = "yinxiang";
+                    break;
+                case "youdaonote":
+                    name = "youdaonote";
+                    break;
+                case "zip":
+                    name = "zip";
+                    break;
             }
-            return $"{DataSource.ApiUserMeData.Config.CdnRoot}image/icons/{name}.png";
+            return $"/Assets/Images/Icons/{name}.png";
         }
 
         /// <summary>
