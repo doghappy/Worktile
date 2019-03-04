1f4161db78882f8b4e2fd25cfa3e7304e4fffae4
HeroWong <hero_wong@outlook.com>
2019-03-04 23:28:02 +0800

    to do comment

--- Worktile/ViewModels/Message/Detail/Content/MessageBaseViewModel.cs
+++ Worktile/ViewModels/Message/Detail/Content/MessageBaseViewModel.cs
@@ -14,5 +14,10 @@ namespace Worktile.ViewModels.Message.Detail.Content
 
         protected int RefType => Session.PageType == PageType.Channel ? 1 : 2;
         protected abstract string IdType { get; }
+        
+        // To do:
+        // 1. 将 ViewModel 和 Views 放置到 Read 和 UnRead 文件夹
+        // 2. 写一个中间类 FileMessageViewModel，派生自此 MessageBaseViewModel
+        // MessageViewModel 和 FileViewModel 类继承 FileMessageViewModel
     }
 }
