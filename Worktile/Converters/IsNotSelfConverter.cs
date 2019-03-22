7977f19ff48e0df119ac9900e491871b525f46bd
HeroWong <hero_wong@outlook.com>
2019-03-22 14:34:26 +0800

    disable remove member self

---
+++ Worktile/Converters/IsNotSelfConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Worktile.Common;
+
+namespace Worktile.Converters
+{
+    public class IsNotSelfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString() != DataSource.ApiUserMeData.Me.Uid;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
