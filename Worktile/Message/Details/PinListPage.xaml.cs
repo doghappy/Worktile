93e1b74e4695c48106a5816d680e506a71b5b653
HeroWong <hero_wong@outlook.com>
2019-07-08 14:39:17 +0800

    Pin list support image viewer

--- Worktile/Message/Details/PinListPage.xaml.cs
+++ Worktile/Message/Details/PinListPage.xaml.cs
@@ -1,7 +1,15 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using System.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Worktile.Common;
 using Worktile.Models;
+using Worktile.Tool;
+using Worktile.Tool.Models;
 using WtMessage = Worktile.Message.Models;
 
 namespace Worktile.Message.Details
@@ -26,6 +34,31 @@ namespace Worktile.Message.Details
             var btn = sender as Button;
             var msg = btn.DataContext as WtMessage.Message;
             await ViewModel.UnPinAsync(msg);
+        }
+
+        private async void MessageControl_OnImageMessageClick(WtMessage.Message message)
+        {
+            var data = new ImageViewerData
+            {
+                SelectedItem = UtilityTool.GetS3FileUrl(message.Body.Attachment.Id),
+                ItemSource = ViewModel.Messages
+                    .Where(m => m.Type == WtMessage.MessageType.Image)
+                    .Select(m => UtilityTool.GetS3FileUrl(m.Body.Attachment.Id))
+                    .ToList()
+            };
+
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            int newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame frame = new Frame();
+                frame.Navigate(typeof(ImageViewerPage), data);
+                Window.Current.Content = frame;
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
         }
     }
 }
