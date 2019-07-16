0fd1fa9964f2ec6bbd2dc05f860f7a628114179f
HeroWong <hero_wong@outlook.com>
2019-07-16 11:17:23 +0800

    star unstar delete session

--- Worktile/Message/MessagePage.xaml.cs
+++ Worktile/Message/MessagePage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Worktile.Main;
+using Worktile.Models;
 
 namespace Worktile.Message
 {
@@ -17,6 +20,28 @@ namespace Worktile.Message
         {
             ViewModel.Frame = ContentFrame;
             MainViewModel.MessageViewModel = ViewModel;
+        }
+
+        private async void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.DeleteRightTappedSessionAsync();
+        }
+
+        private void ListView_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            var listView = sender as ListView;
+            ListViewItemMenuFlyout.ShowAt(listView, e.GetPosition(listView));
+            ViewModel.RightTappedSession = ((FrameworkElement)e.OriginalSource).DataContext as Session;
+        }
+
+        private async void StarButton_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.StarAsync();
+        }
+
+        private async void UnStarButton_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.UnStarAsync();
         }
     }
 }
--- Worktile/Message/MessageViewModel.cs
+++ Worktile/Message/MessageViewModel.cs
@@ -3,6 +3,8 @@ using Worktile.Main;
 using Worktile.Models;
 using Worktile.Common;
 using Windows.UI.Xaml.Controls;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Worktile.Message
 {
@@ -23,12 +25,67 @@ namespace Worktile.Message
             }
         }
 
+        private Session _rightTappedSession;
+        public Session RightTappedSession
+        {
+            get => _rightTappedSession;
+            set => SetProperty(ref _rightTappedSession, value);
+        }
+
+
         public Frame Frame { get; set; }
+
+        private string RightTappedSessionType => RightTappedSession.Type.ToString().ToLower() + "s";
 
         public void Highlight(Session session)
         {
             _session = session;
             OnPropertyChanged(nameof(Session));
+        }
+
+        public async Task StarAsync()
+        {
+            string url = $"api/{RightTappedSessionType}/{RightTappedSession.Id}/star";
+            var data = await WtHttpClient.PutAsync(url);
+            if (data.Value<int>("code") == 200 && data.Value<bool>("data"))
+            {
+                RightTappedSession.IsStar = true;
+            }
+        }
+
+        public async Task UnStarAsync()
+        {
+            string url = $"api/{RightTappedSessionType}/{RightTappedSession.Id}/unstar";
+            var data = await WtHttpClient.PutAsync(url);
+            if (data.Value<int>("code") == 200 && data.Value<bool>("data"))
+            {
+                RightTappedSession.IsStar = false;
+            }
+        }
+
+        public async Task DeleteRightTappedSessionAsync()
+        {
+            if (Sessions.Count > 1)
+            {
+                bool success;
+                if (RightTappedSession.Type == SessionType.Channel)
+                {
+                    string url = $"api/channels/{RightTappedSession.Id}/disable";
+                    var data = await WtHttpClient.PutAsync(url);
+                    success = data.Value<int>("code") == 200;
+                }
+                else
+                {
+                    string url = $"api/sessions/{RightTappedSession.Id}";
+                    var data = await WtHttpClient.DeleteAsync(url);
+                    success = data.Value<int>("code") == 200 && data.Value<bool>("data");
+                }
+                if (success)
+                {
+                    Sessions.Remove(RightTappedSession);
+                    Session = Sessions.First();
+                }
+            }
         }
     }
 }
