d2587d54ce16b968756ff7569637ed206f982757
HeroWong <hero_wong@outlook.com>
2019-07-16 16:31:32 +0800

    add multi lang

--- Worktile/Message/Dialogs/CreateGroupDialog.xaml.cs
+++ Worktile/Message/Dialogs/CreateGroupDialog.xaml.cs
@@ -82,9 +82,76 @@ namespace Worktile.Message.Dialogs
             }
         }
 
+        private bool _showError;
+        public bool ShowError
+        {
+            get => _showError;
+            set
+            {
+                if (_showError != value)
+                {
+                    _showError = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShowError)));
+                }
+            }
+        }
+
+        private string _errorText;
+        public string ErrorText
+        {
+            get => _errorText;
+            set
+            {
+                if (_errorText != value)
+                {
+                    _errorText = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ErrorText)));
+                }
+            }
+        }
+
         private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-
+            IsActive = true;
+            ShowError = false;
+            if (string.IsNullOrWhiteSpace(ChannelName))
+            {
+                ErrorText = UtilityTool.GetStringFromResources("ThisOptionIsRequired");
+                ShowError = true;
+                GroupNameTextBox.Focus(FocusState.Programmatic);
+                args.Cancel = true;
+            }
+            else
+            {
+                //    var avatars = Avatars.Select(a => a.Id);
+                //    string uids = string.Join(',', avatars);
+                //    var visibility = IsPrivate ? WtVisibility.Private : WtVisibility.Public;
+                //    var data = await _messageService.CreateChannelAsync(uids, ChannelName.Trim(), Color, Description.Trim(), visibility);
+                //    if (data.Code == 200)
+                //    {
+                //        for (int i = 0; i < 10; i++)
+                //        {
+                //            await Task.Delay(20);
+                //            var firstSession = _masterPage.Sessions.First();
+                //            if (firstSession.Id == data.Data.Id && _masterPage.SelectedSession != firstSession)
+                //            {
+                //                _masterPage.SelectedSession = firstSession;
+                //                break;
+                //            }
+                //        }
+                //    }
+                //    else if (data.Code == 3004)
+                //    {
+                //        ErrorText = "已有同名群组";
+                //        ShowError = true;
+                //        ChannelNameTextBox.Focus(FocusState.Programmatic);
+                //    }
+                //    else
+                //    {
+                //        ErrorText = "创建失败";
+                //        ShowError = true;
+                //    }
+            }
         }
     }
 }
